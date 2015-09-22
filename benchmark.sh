3dbd4e4bec61f2a062bb7d918fde09f7fc5d0e92
Bartek Zdanowski <bzd@touk.pl>
2015-09-22 18:18:29 +0200

    forgot to comment out other tests

--- benchmark.sh
+++ benchmark.sh
@@ -57,6 +57,6 @@ $JAVA_HOME/bin/java \
 -server -XX:+TieredCompilation -XX:+AggressiveOpts \
 -jar \"$uberJar\" \
 -jvmArgsAppend \"-DworkerCount=$workerCount -DringSize=$ringSize -javaagent:$quasarJar\" \
--wi 5 -i 10 -bm avgt -tu ms -f 5 \".*VertxRingBenchmark.*\""
+-wi 5 -i 10 -bm avgt -tu ms -f 5 \".*RingBenchmark.*\""
 echo "$cmd"
 eval "$cmd"
